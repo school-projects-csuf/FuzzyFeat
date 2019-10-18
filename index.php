4b16250c9b709a878eda3fc8368d02fc05968547
Joesh Bautista <joeshbautista@csu.fullerton.edu>
2019-10-18 16:15:28 -0700

    login functionality updates

--- index.php
+++ index.php
@@ -95,14 +95,14 @@
           </div>
           <div class="modal-body">
             <!-- log in form -->
-            <form>
+            <form action="includes/login.inc.php" method="POST">
               <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
+                <label for="uid">Username or Email address</label>
+                <input type="text" name="uid" class="form-control" placeholder="Enter username or email" required>
               </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
               </div>
               <div class="form-group form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
